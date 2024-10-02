c0007b1e0af2626946484ed0b34cdfaa25cd32b6
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2024-10-02 18:49:55 -0400

    using for loop to add sections

--- javascript/app.js
+++ javascript/app.js
@@ -5,3 +5,21 @@ const ul = document.getElementById('navbar__list');
 const divSections = document.getElementsByClassName('landing_page');
 const sectionTags = document.getElementsByTagName('section');
 //
+
+
+for(let i = 0; i<divSections.length; i++) {
+  
+  const sectionId = sectionTags[i].id
+  const sectionAttributes = divSections[i].childNodes[1].textContent.trim()
+  const li = document.createElement('li') ;
+  const a = document.createElement('a');
+  
+  
+  
+  a.textContent = `${sectionAttributes}`
+  a.href=`#${sectionId}`
+  
+  
+  li.appendChild(a);
+  ul.appendChild(li); 
+}
