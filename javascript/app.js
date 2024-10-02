4c75cab6f0dfc168e165163a021fdcc7371dee4c
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2024-10-02 18:40:50 -0400

    create global variables

---
+++ javascript/app.js
@@ -0,0 +1,7 @@
+// Global Variables
+const navHeader = document.querySelector('.page__header')
+const nav = document.getElementsByClassName('navbar__menu');
+const ul = document.getElementById('navbar__list');
+const divSections = document.getElementsByClassName('landing_page');
+const sectionTags = document.getElementsByTagName('section');
+//
