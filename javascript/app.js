03bab3b766375c6614d4cfee2fabc21d05d974c8
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2024-10-02 22:48:54 -0400

    building navigation and adding active class

--- javascript/app.js
+++ javascript/app.js
@@ -1,25 +1,41 @@
 // Global Variables
-const navHeader = document.querySelector('.page__header')
+const navHeader = document.querySelector('.page__header');
 const nav = document.getElementsByClassName('navbar__menu');
 const ul = document.getElementById('navbar__list');
 const divSections = document.getElementsByClassName('landing_page');
-const sectionTags = document.getElementsByTagName('section');
+const sections = document.querySelectorAll("section");
 //
 
 
-for(let i = 0; i<divSections.length; i++) {
-  
-  const sectionId = sectionTags[i].id
-  const sectionAttributes = divSections[i].childNodes[1].textContent.trim()
+for(const section of sections) {
+  //create <li> for the <ul>//
   const li = document.createElement('li') ;
+  //create 'a href' to scroll to each section
   const a = document.createElement('a');
   
-  
-  
-  a.textContent = `${sectionAttributes}`
-  a.href=`#${sectionId}`
+  a.textContent = section.dataset.nav
+  a.href=`#${section.id}`
   
   
   li.appendChild(a);
   ul.appendChild(li); 
 }
+
+function makeActive() {
+  const VALUE = 150;
+  const navLinks = document.querySelectorAll('nav a');
+
+  sections.forEach((section, index) => {
+      const box = section.getBoundingClientRect();
+      if (box.top <= VALUE && box.bottom >= VALUE) {
+          section.classList.add('active'); // Add active class to the section
+          navLinks[index].classList.add('active'); // Add active class to the corresponding nav link
+      } else {
+          section.classList.remove('active'); // Remove active class from the section
+          navLinks[index].classList.remove('active'); // Remove active class from the corresponding nav link
+      }
+  });
+};
+
+// Call the function on scroll
+document.addEventListener('scroll', makeActive);
